eb0f3a9752ba1d900a24b0bdc114f793137bd2ab
tobi <tobias.moll@redstripes.de>
2018-09-06 17:50:02 +0200

    * new post dialog überarbeitet

--- app/src/main/java/de/redstripes/schwasenphrein/fragments/MainFragment.kt
+++ app/src/main/java/de/redstripes/schwasenphrein/fragments/MainFragment.kt
@@ -1,10 +1,12 @@
 package de.redstripes.schwasenphrein.fragments
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerAdapter
@@ -12,12 +14,17 @@ import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import de.redstripes.schwasenphrein.R
+import de.redstripes.schwasenphrein.helpers.Helper
 import de.redstripes.schwasenphrein.models.Post
 import de.redstripes.schwasenphrein.viewholder.PostViewHolder
 import kotlinx.android.synthetic.main.fragment_main.view.*
+import kotlinx.android.synthetic.main.fragment_new_post.view.*
 import kotlinx.android.synthetic.main.fragment_rating.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
+import org.jetbrains.anko.warn
+import org.threeten.bp.LocalDateTime
+import java.util.*
 
 class MainFragment : Fragment(), AnkoLogger {
 
@@ -47,10 +54,7 @@ class MainFragment : Fragment(), AnkoLogger {
         recyclerView?.setHasFixedSize(true)
 
         rootView.main_fab_add_post.setOnClickListener {
-            val newPostFragment = NewPostFragment()
-            val transaction = childFragmentManager.beginTransaction()
-            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            transaction.add(R.id.main_container, newPostFragment).addToBackStack(null).commit()
+            onFabClicked()
         }
 
         return rootView
@@ -76,6 +80,62 @@ class MainFragment : Fragment(), AnkoLogger {
                 .build()
         adapter = MainFragmentRecyclerAdapter(options, activity!!, database!!, ::onStarClicked)
         recyclerView?.adapter = adapter
+    }
+
+    private fun onFabClicked() {
+        val view = layoutInflater.inflate(R.layout.fragment_new_post, parent, false)
+        ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, resources.getStringArray(R.array.autocomplete_persons)).also { view.new_post_person.setAdapter(it) }
+
+        AlertDialog.Builder(requireContext())
+                .setMessage(getString(R.string.title_new_post))
+                .setView(view)
+                .setCancelable(true)
+                .setPositiveButton(getString(R.string.cmd_add)) { _, _ ->
+                    val person = view.new_post_person.text.toString()
+                    val text = view.new_post_text.text.toString()
+
+                    if (TextUtils.isEmpty(person)) {
+                        view.new_post_person.error = getString(R.string.info_required)
+                    }
+
+                    if (TextUtils.isEmpty(text)) {
+                        view.new_post_text.error = getString(R.string.info_required)
+                        return@setPositiveButton
+                    }
+
+                    addNewPost(person, text)
+                }
+                .setNegativeButton(getString(R.string.cmd_cancel)) { _, _ -> }
+                .create()
+                .show()
+    }
+
+    private fun addNewPost(person: String, text: String) {
+        Toast.makeText(activity, getString(R.string.status_posting), Toast.LENGTH_SHORT).show()
+
+        val uid = getUid()
+        val database = FirebaseDatabase.getInstance().reference
+        database.child("users").child(uid).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue<de.redstripes.schwasenphrein.models.User>(de.redstripes.schwasenphrein.models.User::class.java)
+
+                if (user == null) {
+                    Toast.makeText(activity, getString(R.string.error_could_not_get_user), Toast.LENGTH_SHORT).show()
+                    error("User $uid is unexpectedly null")
+                } else {
+                    val key = database.child("posts").push().key
+                    val post = Post(uid, person, text, Helper.dateToString(LocalDateTime.now()))
+                    val postValues = post.toMap()
+                    val childUpdates = HashMap<String, Any>()
+                    childUpdates["/posts/$key"] = postValues
+                    database.updateChildren(childUpdates)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                warn("getUser:onCancelled " + error.toException())
+            }
+        })
     }
 
     private fun onStarClicked(postRef: DatabaseReference, userRating: Float) {
